7f470ca4ae50af4c57dd8081a587ee3577fe502f
Antoine Barrault <barrault01@gmail.com>
2022-03-15 10:04:05 +0100

    Initial Commit

---
+++ CombinePublishers/CombinePublishersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinePublishersApp.swift
+//  CombinePublishers
+//
+//  Created by Antoine Barrault on 15/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CombinePublishersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
