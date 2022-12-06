154196c2accc2d477e00cb751e4573fba3c9f84e
Antoine Barrault <barrault01@gmail.com>
2022-12-06 07:07:54 +0100

    first commit

--- CombinePublishers/ContentView.swift
+++ CombinePublishers/ContentView.swift
@@ -6,16 +6,43 @@
 //
 
 import SwiftUI
+import Combine
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+class User: ObservableObject {
+    @Published var username: String = ""
+    @Published var password: String = ""
+    @Published var protectWithTouchID = false
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+
+    @ObservedObject var user = User()
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Username:")
+                TextField("", text: $user.username)
+                    .textFieldStyle(.roundedBorder)
+            }
+            .padding()
+            HStack {
+                Text("Password:")
+                SecureField("", text: $user.password)
+                    .textFieldStyle(.roundedBorder)
+            }
+            .padding()
+            Toggle("Protect with Face ID", isOn: $user.protectWithTouchID)
+                .padding()
+
+        }
+        .onReceive(
+            Publishers.CombineLatest3(user.$username,
+                                      user.$password,
+                                      user.$protectWithTouchID)) { value in
+            // here you can do what you want with the value
+            // you will receive a tuple like this($username, $password, $protectWithTouchID)
+                print(value)
+        }
     }
 }
